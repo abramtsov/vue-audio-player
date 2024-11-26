400d1b4cab354904bd342caf6fe2cd232ff4d7b3
Vladimir Abramtsov <abramtsov.vn@gmail.com>
2024-11-26 17:12:56 +0300

    create

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <img src="../assets/phone.png" alt="" />
+    <div class="player">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum aliquid
+      animi nam accusamus libero labore laborum tenetur error, architecto
+      necessitatibus aperiam ea doloremque numquam excepturi aut sequi
+      consectetur facilis corporis.
+    </div>
+  </div>
+</template>
+
